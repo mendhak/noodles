15f68876a7fe19b8444a51cf9beaed9f6860ef3f
mendhak <mendhak@users.noreply.github.com>
2022-10-28 21:37:32 +0100

    ignore README in 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -10,13 +10,20 @@ module.exports = (function(eleventyConfig){
         return sections = collectionApi.getFilteredByGlob('noodles/*.*');
     });
 
+    // Copies the images files straight into the output folder, so that the HTML can reference it. 
+    // Without this, the images get copied to output/noodles/images/ instead of output/images/.  
+    // This allows the MD to reference the image files and the output HTML too. 
     eleventyConfig.addPassthroughCopy({ "noodles/images": "images" });
 
+    //Don't process README.md, that's for me!
+    eleventyConfig.ignores.add("README.md");
+
+    // Config values that could be passed via arguments but it's just easier in here. 
     return {
         dir: {
           input: ".",
           output: "output",
-          templateFormats: ["html", "md"]
+          templateFormats: ["html"]
         }
     }
 
